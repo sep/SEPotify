862c2cff104f8136148d256f67365fef1073f9df
Will Trimble <wmtrimble@sep.com>
2024-07-20 20:16:11 -0400

    Auth

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,30 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { redirectToAuthCodeFlow, getAccessToken } from "./authCodeWithPkce";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+const clientId = "your-client-id";
+
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "SEPotify" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('SEPotify.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello Worlds from SEPotify!');
+	const disposable = vscode.commands.registerCommand('SEPotify.login', () => {
+		redirectToAuthCodeFlow(clientId, context);
 	});
+
+	const handleUri = async (uri: vscode.Uri) => {
+		const queryParams = new URLSearchParams(uri.query);
+
+		if (queryParams.has('code')) {
+			await getAccessToken(clientId, queryParams.get('code')!, context);
+			vscode.window.showInformationMessage('Login successful');
+		}
+	}
+
+	context.subscriptions.push(
+		vscode.window.registerUriHandler({
+			handleUri
+		})
+	);
 
 	context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
